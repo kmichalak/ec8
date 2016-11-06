accc1200397ad2428ee42ac5c5e92979c11924a2
Kamil Michalak <kmichalak8@gmail.com>
2016-11-06 23:04:37 +0100

    Define CHIP-8's memory and basic registers

--- ck8.c
+++ ck8.c
@@ -1,8 +1,25 @@
+#include "ck8.h"
+
+void initialize() {
+	// initialize registers
+	// setup memory 
+}
+
+void emulate_cycle() {
+	// fetch opcode
+		// decode 
+	// execute 
+	// update timers
+}
+
+
+
 int main(int argc, char* argv[]) {
 
-
 	while(1) {
-		
+		// emulate cycle
+		// draw graphics if draw flag is set
+		// set keys
 	}
 
 	return 0;
