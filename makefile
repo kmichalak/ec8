ea20861a003b579ad5abb8688c1e91414edcf949
Kamil Michalak <kmichalak8@gmail.com>
2016-11-07 21:15:37 +0100

    Move opcodes handlers to the separated object.
    Extract opcodes handlers as two files opcodes.h and opcodes.c.

--- makefile
+++ makefile
@@ -1,16 +1,30 @@
 CC = gcc
 CFLAGS = -g -Wall
+
 TARGET = ck8
-MAIN = ck8.c
+MAIN = ck8
+OPCODES = opcodes
+
 
 all: $(TARGET)
 
-$(TARGET): $(MAIN)
-	$(CC) $(CFLAGS) -o $(TARGET) $(MAIN)
+
+$(TARGET): $(MAIN).o $(OPCODES).o
+	$(CC) $(CFLAGS) -o $(TARGET) $(MAIN).o $(OPCODES).o
+
+
+$(MAIN).o: $(MAIN).c $(OPCODES).h
+	$(CC) $(CFLAGS) -c $(MAIN).c
+
+
+$(OPCODES).o: $(OPCODES).c $(OPCODES).h
+	$(CC) $(CFLAGS) -c $(OPCODES).c
+
 
 test: 
 	$(MAKE) -C tests
 
+
 clean:
-	$(RM) $(TARGET)
+	$(RM) $(TARGET) $(MAIN).o $(OPCODES).o
 	$(MAKE) clean -C tests
---
+++ opcodes.h
@@ -0,0 +1,58 @@
+#ifndef OPCODES_H
+#define OPCODES_H
+
+// 00EX
+void handle_0(unsigned short opcode);
+
+// 00E0
+void clear_screen(unsigned short opcode);
+
+// 00EE
+void return_from_rsubutine(unsigned short opcode);
+
+// 1NNN
+void jump(unsigned short opcoder);
+
+// 2NNN
+void call_subrutine(unsigned short opcode);
+
+// 3XNN
+void vx_equals(unsigned short opcode);
+
+// 4XNN
+void vx_not_equals(unsigned short opcode);
+
+// 5XY0
+void vx_equals_vy(unsigned short opcode);
+
+// 6XNN
+void set_vx(unsigned short opcode);
+
+// 7XNN
+void add_to_vx(unsigned short opcode);
+
+// 8XYN
+void handle_8(unsigned short opcode);
+
+// 9XY0
+void vx_not_equals_vy(unsigned short opcode);
+
+// ANNN
+void set_prog_counter(unsigned short opcode);
+
+// BNNN
+void offset_jump(unsigned short opcode);
+
+// CXNN
+void random_vx(unsigned short opcode);
+
+// DXYN
+void draw(unsigned short opcode);
+
+// EXNN
+void handle_key(unsigned short opcode);
+
+// FXNN
+void handle_f(unsigned short opcode);
+
+#endif 
