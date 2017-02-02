5a58ee8df0a342dff729f665b9f5fa86e09d14a5
Kamil Michalak <kmichalak8@gmail.com>
2017-02-02 15:29:47 +0100

    Remove commented code from cpu.h

--- cpu.h
+++ cpu.h
@@ -16,8 +16,6 @@ struct Cpu {
 	unsigned short stack[16];
 	unsigned short sp;
 	unsigned char key[16];
-	// opcode_handler handlers[16];
-	// int (*run_cycle)(Cpu *cpu);
 	void (*fetch_opcode)(Cpu *cpu);
 	void (*handle_opcode)(Cpu *cpu);
 
